eb34fc535bbf1dfa0af2a7bbae77cc651cb1bee7
Priyanshu <2003priyanshusingh@gmail.com>
2023-07-27 17:22:18 +0530

    Task questining

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:luma/pallets.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required String title});
@@ -17,7 +18,61 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
-        leading: const Icon(Icons.menu),
+        leading: const Icon(Icons.menu, color: Colors.black),
+      ),
+      body: Column(
+        children: [
+          Stack(children: [
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                height: 120,
+                width: 120,
+                margin: const EdgeInsets.only(top: 4),
+                decoration: const BoxDecoration(
+                  color: Pallete.assistantCircleColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            Container(
+              height: 123,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets\images\va.jpg'),
+                ),
+              ),
+            )
+          ]),
+          //chat bubble
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ), // EdgeInsets.symmetric
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Hi Buddy, what task can I do for you?',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
